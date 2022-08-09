63e89c18a28727c0e200e07ca7024e3a398a05a9
Thailson Bezerra <thailsonbezerra@outlook.com>
2022-08-09 15:28:23 -0300

    adicionado transicao do background color

--- js/slideColor.js
+++ js/slideColor.js
@@ -1,4 +1,4 @@
-const colors = {
+export const colors = {
   gray: '#898886',
   blue: '#00aef6',
   green: '#09b500',
@@ -20,12 +20,22 @@ export const chameleonColorSlide = () => {
         .replace('img/camaleos/', '')
         .replace('.png', '');
 
-      changeColor(chameleonColor);
+      changeColor(chameleonColor, chameleonImg, chameleon);
     }
   });
 };
 
-export const changeColor = (color) => {
+export const changeColor = (color, chameleonImg, chameleon) => {
   const slide = document.querySelector('.slide-wrapper');
+  slide.style.transition = 'background-color 0s';
   slide.style.backgroundColor = colors[color];
+  setTimeout(() => {
+    slide.style.transition = 'background-color 2s';
+    slide.style.backgroundColor = '#000';
+
+    setTimeout(() => {
+      chameleon.querySelector('img').attributes.src.value =
+        chameleonImg.replace('camaleos', 'bgBlack');
+    }, 2000);
+  }, 3000);
 };
