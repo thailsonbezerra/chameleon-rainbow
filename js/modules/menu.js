3613ec8e6bf1dca0bb4550857d05fa3520ed5ee0
Thailson Bezerra <thailsonbezerra@outlook.com>
2022-09-26 16:53:02 -0300

    toggle menu ui modificado

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,24 +1,34 @@
 export const menu = () => {
-  const home = document.querySelector(".home");
-  const slide = document.querySelector("slide-wrapper");
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu-bg');
+  const home = document.querySelector('.home');
+  const slide = document.querySelector('.slide-wrapper');
+  const hamburguerInner = document.querySelector('.hamburger-inner');
+
   const pagsArray = [home, slide];
   const handleClickMenu = () => {
-    hamburger.classList.toggle("is-active");
-    if (hamburger.classList.contains("is-active")) {
+    hamburger.classList.toggle('is-active');
+    hamburger.classList.toggle('color-change-7x');
+    hamburger.style.background = '#000';
+
+    if (hamburger.classList.contains('is-active')) {
+      menu.style.display = 'block';
+      menu.classList.add('slide-in-blurred-tr');
       pagsArray.map((el) => {
         if (el) {
-          el.style.display = "none";
+          el.style.display = 'none';
         }
       });
     } else {
+      menu.style.display = 'none';
+      menu.classList.remove('slide-in-blurred-tr');
       pagsArray.map((el) => {
         if (el) {
-          el.style.display = "block";
+          el.style.display = 'block';
         }
       });
     }
   };
 
-  const hamburger = document.querySelector(".hamburger");
-  hamburger.addEventListener("click", handleClickMenu);
+  hamburger.addEventListener('click', handleClickMenu);
 };
